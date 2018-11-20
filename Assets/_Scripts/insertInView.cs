30b58238b8768c2e1668fe28d50b498bdb45067b
Carlos Salas <ultima_lux@hotmail.com>
2018-11-20 14:27:29 -0700

    photonView
    photon view added to soda

---
+++ Assets/_Scripts/insertInView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+
+public class insertInView : MonoBehaviour {
+
+
+    private void Start()
+    {
+        transform.GetComponent<PhotonView>().ObservedComponents.Add(transform);
+    }
+
+}
