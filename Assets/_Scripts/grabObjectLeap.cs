b8e1773e1b352261d9554bd49ea811eed49d4931
Carlos Salas <ultima_lux@hotmail.com>
2018-11-29 12:33:04 -0700

    LeapAdded
    Use Oxxowithhands 1

---
+++ Assets/_Scripts/grabObjectLeap.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class grabObjectLeap : MonoBehaviour {
+
+    [SerializeField] private CapsuleHand capsuleHandRight;
+    [SerializeField] private CapsuleHand capsuleHandLeft;
+
+    private Hand handRight;
+    private Hand handLeft;
+
+    private int handRightID;
+    private int handLeftID;
+
+    [SerializeField] private remoteTrigger triggerHandRight;
+    [SerializeField] private remoteTrigger triggerHandLeft;
+
+    private bool isGrabbingRight;
+    private bool isGrabbingLeft;
+
+    [SerializeField] private float sphereRadius;
+
+
+    private void Start()
+    {
+        handRight = capsuleHandRight.GetLeapHand();
+        handLeft = capsuleHandLeft.GetLeapHand();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (capsuleHandRight.gameObject.GetActive())
+        {
+            handRight = capsuleHandRight.GetLeapHand();
+            if (handRight != null) 
+            {
+
+                isGrabbingRight = true;
+                foreach (Finger f in handRight.Fingers)
+                {
+                    if (f.IsExtended)
+                    {
+                        isGrabbingRight = false;
+                        break;
+                    }
+                }
+
+                triggerHandRight.transform.position = handRight.PalmPosition.ToVector3();
+                if (triggerHandRight.isCollidingWithTag("product")) {
+                    if (isGrabbingRight)
+                    {
+                        Transform lic = triggerHandRight.LastItemCollided;
+                        lic.position = handRight.PalmPosition.ToVector3();
+                        lic.GetComponent<Rigidbody>().useGravity = false;
+                    } else triggerHandRight.LastItemCollided.GetComponent<Rigidbody>().useGravity = true;
+                }
+            }
+        }
+
+        if (capsuleHandLeft.gameObject.GetActive())
+        {
+            handLeft = capsuleHandLeft.GetLeapHand();
+            if(handLeft != null)
+            {
+
+                isGrabbingLeft = true;
+                foreach (Finger f in handLeft.Fingers)
+                {
+                    if (f.IsExtended)
+                    {
+                        isGrabbingLeft = false;
+                        break;
+                    }
+                }
+
+                triggerHandLeft.transform.position = handLeft.PalmPosition.ToVector3();
+                if (triggerHandLeft.isCollidingWithTag("product"))
+                {
+                    if (isGrabbingLeft)
+                    {
+                        Transform lic = triggerHandLeft.LastItemCollided;
+                        lic.position = handLeft.PalmPosition.ToVector3();
+                        lic.GetComponent<Rigidbody>().useGravity = false;
+                    }
+                    else triggerHandLeft.LastItemCollided.GetComponent<Rigidbody>().useGravity = true;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (capsuleHandRight.gameObject.GetActive())
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(handRight.PalmPosition.ToVector3(), sphereRadius);
+        }
+
+        if (capsuleHandLeft.gameObject.GetActive())
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(handLeft.PalmPosition.ToVector3(), sphereRadius);
+        }
+    }
+}
---
+++ Assets/_Scripts/remoteTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+
+public class remoteTrigger : MonoBehaviour {
+
+    private Transform lastItemCollided;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lastItemCollided = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (lastItemCollided == other.transform) lastItemCollided = null;
+    }
+
+    public Transform lastItemCollidedWithTag(string Tag)
+    {
+        if (lastItemCollided.CompareTag(Tag)) return lastItemCollided;
+        else return null;
+    }
+
+    public Transform LastItemCollided
+    {
+        get
+        {
+            return lastItemCollided;
+        }
+    }
+
+    public bool isColliding
+    {
+        get
+        {
+            return (lastItemCollided != null);
+        }
+    }
+
+    public bool isCollidingWithTag(string Tag)
+    {
+        if(lastItemCollided) if (lastItemCollided.CompareTag(Tag)) return true;
+        return false;
+    }
+}
